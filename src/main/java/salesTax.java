4b69f4202c02ee0af3623aed60b350aea73a9c2b
trent <trent@DESKTOP-NKK8L7L>
2019-09-08 21:02:48 -0500

    Finished Project

--- src/main/java/salesTax.java
+++ src/main/java/salesTax.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,19 @@
  */
 public class salesTax {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter the cost purchased");
+        double purchase = keyboard.nextDouble();
+        double stateSalesTax = purchase * 0.04;
+        double countrySalesTax = purchase * 0.02;
+        double totalSalesTax = countrySalesTax + stateSalesTax;
+        double totalSale = purchase + stateSalesTax + countrySalesTax;
+        
+        System.out.println("The amount purchased is $" + purchase);
+        System.out.println("State Sales Tax is equal to $" + stateSalesTax);
+        System.out.println("Country Sales Tax is equal to $" + stateSalesTax);
+        System.out.println("The total sales tax is $" + totalSalesTax);
+        System.out.println("The total cost of sale is $" + totalSale);
         
     }
     
